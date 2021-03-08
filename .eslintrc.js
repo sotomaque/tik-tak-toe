6a206c2d6f5cf3e2bb8e234ca134d043f36cc076
Enrique <sotomaque@gmail.com>
2021-03-07 19:06:36 -0800

    ...

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {},
+};
