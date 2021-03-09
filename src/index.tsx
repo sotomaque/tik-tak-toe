5bc8cb22c16e2ee185ae46e3d50cffb4562df127
Enrique <sotomaque@gmail.com>
2021-03-08 21:05:33 -0800

    imported custom font

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,24 @@
 import React from 'react';
-import { StyleSheet, View, Image } from 'react-native';
-import { Home, Game } from '@screens';
+import { StyleSheet, View, Text } from 'react-native';
+import {
+  useFonts,
+  DeliusUnicase_400Regular,
+  DeliusUnicase_700Bold,
+} from '@expo-google-fonts/delius-unicase';
+import { AppLoading } from 'expo';
 
 export default function App() {
+  const [fontLoaded] = useFonts({
+    DeliusUnicase_400Regular,
+    DeliusUnicase_700Bold,
+  });
+  if (!fontLoaded) return <AppLoading />;
+
   return (
     <View style={styles.container}>
-      <Image source={require('@assets/icon.png')} />
-      <Home />
-      <Game />
+      <Text style={{ fontFamily: 'DeliusUnicase_400Regular', fontSize: 25 }}>
+        Hello World
+      </Text>
     </View>
   );
 }
