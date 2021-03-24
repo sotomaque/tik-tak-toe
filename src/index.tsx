a02a49e18c6ceb1fe75ed017c14761b22c614a76
Enrique <sotomaque@gmail.com>
2021-03-23 22:32:06 -0700

    amplify config

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,10 @@ import { AppBoostrap } from '@components';
 import Navigator from '@config/navigator';
 import { SettingsProvider } from '@context/settings-context';
 
+import Amplify from 'aws-amplify';
+import config from '../aws-exports';
+Amplify.configure(config);
+
 function App(): ReactElement {
   return (
     <AppBoostrap>
