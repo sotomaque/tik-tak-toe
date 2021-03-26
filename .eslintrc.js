2ee2a15495e1b63f5ee4397b9e7f5a86122861df
Enrique <sotomaque@gmail.com>
2021-03-26 00:00:02 -0700

    chore: added eslint 10x plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,8 +24,19 @@ module.exports = {
       version: 'detect',
     },
   },
-  plugins: ['react', '@typescript-eslint', 'ts-exports'],
+  plugins: ['react', '@typescript-eslint', 'ts-exports', '10x'],
   rules: {
     'ts-exports/unused-exports': 2,
+    '10x/auto-import': [
+      'error',
+      {
+        imports: {
+          useRef: "import {useRef} from 'react'",
+          useEffect: "import {useEffect} from 'react'",
+          useState: "import {useState} from 'react'",
+          useCallback: "import {useCallback} from 'react'",
+        },
+      },
+    ],
   },
 };
